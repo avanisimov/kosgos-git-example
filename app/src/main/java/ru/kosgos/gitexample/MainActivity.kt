ae3622ebe5e98e2a0ad58bc7416761c3decf9e49
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2025-04-18 19:31:16 +0300

    Name is changed from 'Android' to 'Aleksei'

--- app/src/main/java/ru/kosgos/gitexample/MainActivity.kt
+++ app/src/main/java/ru/kosgos/gitexample/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Aleksei",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
